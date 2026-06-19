d995c535d369dbafb59bfa8843b721d5f227dbed
espinorobertopaolo-sys <espinorobertopaolo@gmail.com>
2026-06-19 21:38:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
